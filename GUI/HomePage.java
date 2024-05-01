6b7d8c1226cb1044775a703ea76fd146457313d7
Omar Mohamed <omarcoptan9@gmail.com>
2024-05-01 20:05:19 +0300

    Initial GUI

---
+++ GUI/HomePage.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import javax.swing.*;
+
+public class HomePage {
+        JFrame mainPage = new JFrame("Saleni Chatbot");
+        public HomePage() {
+            mainPage.setLocationRelativeTo(null);
+            mainPage.setTitle("Saleni Chatbot");
+            mainPage.setVisible(true);
+        }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+import GUI.HomePage;
+
+public class Main {
+    public static void main(String[] args) {
+        HomePage homepage = new HomePage();
+    }
+}
